88b68b9a9919659475a2413a1e20d7b3e0985f3c
mildsalmon_local_pc <mildsalmon@gmail.com>
2020-11-10 15:39:24 +0900

    20.11.10-1_ADD SJ1_full_wave.py

--- SJ1_full_wave.py
+++ SJ1_full_wave.py
@@ -4,59 +4,61 @@ import csv
 from matplotlib import pyplot as plt
 import pandas as pd
 
-# def __init__(self):
-#     self.wave_start_point
-#     self.wave_end_point
-#
-#     pd.set_option('display.max_columns', 100)
-#     pd.set_option('display.width', 1000)
-#
-#     self.sj1_csv_people = {
-#         # people 1
-#         1: self.select_people(0),
-#         # people 2
-#         2: self.select_people(1),
-#         # people 3
-#         3: self.select_people(2),
-#         # people 4
-#         4: self.select_people(3),
-#         # people 5
-#         5: self.select_people(4)
-#     }
 
-def select_people(num):
-    sj1_csv = pd.read_csv('data/SJ1.csv', engine='python')
+class SJ1_full_wave:
+    def __init__(self):
+        self.wave_start_point
+        self.wave_end_point
 
-    print(sj1_csv)
-    #
-    # print(sj1_csv.shape)
-    #
-    # print(sj1_csv.dropna(axis=0, how='any'))
+        pd.set_option('display.max_columns', 100)
+        pd.set_option('display.width', 1000)
 
-    sj1_csv_exam = sj1_csv.dropna(axis=0, how='any')
-    sj1_csv_people = sj1_csv_exam.iloc[num, :]
+        self.sj1_csv_people = {
+            # people 1
+            1: self.select_people(0),
+            # people 2
+            2: self.select_people(1),
+            # people 3
+            3: self.select_people(2),
+            # people 4
+            4: self.select_people(3),
+            # people 5
+            5: self.select_people(4)
+        }
 
-    return sj1_csv_people
+    def select_people(self, num):
+        sj1_csv = pd.read_csv('data/SJ1.csv', engine='python')
 
-def setting(sj1_csv_people_select, people_num):
-    if people_num == 1:
-        wave_start_point = 1289
-        wave_end_point = 6505
-    elif people_num == 2:
-        wave_start_point = 5
-        wave_end_point = 0
-    elif people_num == 3:
-        wave_start_point = 1289
-        wave_end_point = 6505
-    elif people_num == 4:
-        wave_start_point = 1289
-        wave_end_point = 6505
-    elif people_num == 5:
-        wave_start_point = 1289
-        wave_end_point = 6505
-    #
-    # self.wave_start_point = wave_start_point
-    # self.wave_end_point = wave_end_point
+        print(sj1_csv)
+        #
+        # print(sj1_csv.shape)
+        #
+        # print(sj1_csv.dropna(axis=0, how='any'))
+
+        sj1_csv_exam = sj1_csv.dropna(axis=0, how='any')
+        sj1_csv_people = sj1_csv_exam.iloc[num, :]
+
+        return sj1_csv_people
+
+    def setting(self, sj1_csv_people_select, people_num):
+        if people_num == 1:
+            wave_start_point = 1289
+            wave_end_point = 6505
+        elif people_num == 2:
+            wave_start_point = 5
+            wave_end_point = 0
+        elif people_num == 3:
+            wave_start_point = 1289
+            wave_end_point = 6505
+        elif people_num == 4:
+            wave_start_point = 1289
+            wave_end_point = 6505
+        elif people_num == 5:
+            wave_start_point = 1289
+            wave_end_point = 6505
+
+        self.wave_start_point = wave_start_point
+        self.wave_end_point = wave_end_point
 
 
 # f = open('data/SJ1.csv', 'r')#, encoding='utf-8')
@@ -76,23 +78,7 @@ if __name__ == "__main__":
     # 판다스 크기 키우기
     # pd.set_option('display.max_rows', 500)
 
-    pd.set_option('display.max_columns', 100)
-    pd.set_option('display.width', 1000)
-
-    sj1_csv_people = {
-        # people 1
-        1: select_people(0),
-        # people 2
-        2: select_people(1),
-        # people 3
-        3: select_people(2),
-        # people 4
-        4: select_people(3),
-        # people 5
-        5: select_people(4)
-    }
-
-    people_num = 5
+    people_num = 2
 
     sj1_csv_people_select = sj1_csv_people[people_num]
 
@@ -112,7 +98,6 @@ if __name__ == "__main__":
     # sj1_csv_people1 = sj1_csv_exam.iloc[4, :]
 
     print(sj1_csv_people_select)
-
 
     biosignal = sj1_csv_people_select[1:5]
     # biowave = sj1_csv_people1[5:6675]
@@ -179,7 +164,7 @@ if __name__ == "__main__":
 
     elif people_num == 2:
         # people2의 wave 30개 선정
-        biowave = sj1_csv_people_select[1:]
+        biowave = sj1_csv_people_select[5:]
         x = range(len(biowave))
 
         print(biosignal)
@@ -187,8 +172,8 @@ if __name__ == "__main__":
 
         print(x)
 
-        # plt.rcParams["figure.figsize"] = (25, 10)
-        # plt.plot(x, biowave)
+        plt.rcParams["figure.figsize"] = (25, 10)
+        plt.plot(x, biowave)
 
         min = biowave.astype(float).min()
         print("min", min)
@@ -205,7 +190,6 @@ if __name__ == "__main__":
 
         x = range(len(biowave))
 
-        plt.rcParams["figure.figsize"] = (25, 10)
         plt.plot(x, biowave)
         plt.axis([0, len(biowave), 0, 1])
         plt.show()
@@ -225,8 +209,8 @@ if __name__ == "__main__":
 
         print(x)
 
-        # plt.rcParams["figure.figsize"] = (25, 10)
-        # plt.plot(x, biowave)
+        plt.rcParams["figure.figsize"] = (25, 10)
+        plt.plot(x, biowave)
 
         min = biowave.astype(float).min()
         print("min", min)
@@ -243,7 +227,6 @@ if __name__ == "__main__":
 
         x = range(len(biowave))
 
-        plt.rcParams["figure.figsize"] = (25, 10)
         plt.plot(x, biowave)
         plt.axis([0, len(biowave), 0, 1])
         plt.show()
@@ -263,8 +246,8 @@ if __name__ == "__main__":
 
         print(x)
 
-        # plt.rcParams["figure.figsize"] = (25, 10)
-        # plt.plot(x, biowave)
+        plt.rcParams["figure.figsize"] = (25, 10)
+        plt.plot(x, biowave)
 
         min = biowave.astype(float).min()
         print("min", min)
@@ -281,7 +264,6 @@ if __name__ == "__main__":
 
         x = range(len(biowave))
 
-        plt.rcParams["figure.figsize"] = (25, 10)
         plt.plot(x, biowave)
         plt.axis([0, len(biowave), 0, 1])
         plt.show()
@@ -302,8 +284,8 @@ if __name__ == "__main__":
 
         print(x)
 
-        # plt.rcParams["figure.figsize"] = (25, 10)
-        # plt.plot(x, biowave)
+        plt.rcParams["figure.figsize"] = (25, 10)
+        plt.plot(x, biowave)
 
         min = biowave.astype(float).min()
         print("min", min)
@@ -320,7 +302,6 @@ if __name__ == "__main__":
 
         x = range(len(biowave))
 
-        plt.rcParams["figure.figsize"] = (25, 10)
         plt.plot(x, biowave)
         plt.axis([0, len(biowave), 0, 1])
         plt.show()
