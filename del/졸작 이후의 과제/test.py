fdc2ee1ef837faf03a62aff3c7daa7d9e627c533
mildsalmon_local_pc <mildsalmon@gmail.com>
2021-09-01 10:42:33 +0900

    untrack

--- del/졸작 이후의 과제/test.py
+++
@@ -1,6 +0,0 @@
-import os
-
-print(os.path.abspath(__file__))
-
-print(os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data")))
-print(os.path.join(os.path.dirname(os.path.abspath(__file__)), "info.csv"))
